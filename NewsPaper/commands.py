a1ecce18489b324a7fe50590dd7acfdb335b31a5
iea32 <iea32@mail.ru>
2022-03-21 19:42:15 +0500

    run commands.py

--- NewsPaper/commands.py
+++ NewsPaper/commands.py
@@ -1,3 +1,12 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NewsPaper.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NewsPaper.settings")
+# os.environ.setdefault("TEAMVAULT_CONFIG_FILE", "/etc/teamvault.cfg")
+
+import django
+django.setup()
+
+
 from django.contrib.auth.models import User
 from news.models import Author, Category, Post, PostCategory, Comment
 
